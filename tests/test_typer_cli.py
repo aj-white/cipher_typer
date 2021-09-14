d7e90d4a4847fd98ccd6ce1ea299efc196bee203
Andrew White <white-aj@outlook.com>
2021-09-14 21:19:49 +0100

    add more tests

--- tests/test_typer_cli.py
+++ tests/test_typer_cli.py
@@ -9,3 +9,25 @@ def test_app():
     result = runner.invoke(app, ["encrypt", "Meet me at 6pm", "23"])
     assert result.exit_code == 0
     assert "+BBQwJBwxQwsMJ" in result.stdout
+
+
+def test_app_decrypt():
+    result = runner.invoke(app, ["decrypt", "+BBQwJBwxQwsMJ", "23"])
+    assert result.exit_code == 0
+    assert "Meet me at 6pm" in result.stdout
+
+
+def test_app_seed():
+    result = runner.invoke(
+        app, ["encrypt", "6000 feet underwater !?", "67", "--method", "seed"]
+    )
+    assert result.exit_code == 0
+    assert "%666}I##p}mw)#&Njp#&};r" in result.stdout
+
+
+def test_app_seed_decrypt():
+    result = runner.invoke(
+        app, ["decrypt", "%666}I##p}mw)#&Njp#&};r", "67", "--method", "seed"]
+    )
+    assert result.exit_code == 0
+    assert "6000 feet underwater !?" in result.stdout
